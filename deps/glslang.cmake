193f5b1f0ce7e1487931dd9eb4d21ac59680c2d8
Ivo van Dongen <info@ivovandongen.nl>
2019-07-19 11:34:04 +0300

    [build] fix glslang header install

--- deps/glslang.cmake
+++ deps/glslang.cmake
@@ -12,8 +12,14 @@ include_vendor_pkg(spirv-tools)
 add_subdirectory(${CMAKE_SOURCE_DIR}/deps/glslang ${CMAKE_BINARY_DIR}/glslang)
 
 # install files in non-standard location for moltenVK
-file(GLOB files RELATIVE ${CMAKE_SOURCE_DIR}/deps/glslang/ "${CMAKE_SOURCE_DIR}/deps/glslang/glslang/**/*.h" "${CMAKE_SOURCE_DIR}/deps/glslang/SPIRV/**/*.h")
+file(GLOB files RELATIVE ${CMAKE_SOURCE_DIR}/deps/glslang/glslang "${CMAKE_SOURCE_DIR}/deps/glslang/glslang/**/*.h")
 foreach (file ${files})
+    configure_file(${CMAKE_SOURCE_DIR}/deps/glslang/glslang/${file} ${CMAKE_BINARY_DIR}/glslang/include/glslang/${file} COPYONLY)
+endforeach ()
+
+file(GLOB spirv_files RELATIVE ${CMAKE_SOURCE_DIR}/deps/glslang "${CMAKE_SOURCE_DIR}/deps/glslang/SPIRV/*.h")
+foreach (file ${spirv_files})
     configure_file(${CMAKE_SOURCE_DIR}/deps/glslang/${file} ${CMAKE_BINARY_DIR}/glslang/include/glslang/${file} COPYONLY)
 endforeach ()
+
 target_include_directories(glslang PUBLIC ${CMAKE_BINARY_DIR}/glslang/include)
